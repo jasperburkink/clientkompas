37e9bdf617b95b34f0d17a0729f85d4632b25ef8
Jasper Burkink <jasper.burkink@specializedbrainsict.nl>
2023-05-08 12:13:02 +0200

    First draft of clean architecture, unitofwork, generic repository Added API en web app

---
+++ CSV-Com/Infrastructure/Persistence/Authentication/AuthenticationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Interfaces.Authentication;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Authentication
+{
+    public class AuthenticationDbContext : DbContext
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options) { }
+    }
+}
