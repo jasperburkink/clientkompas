58e546d4b51df4aa38bda78c2acf131e7f625edb
Jasper Burkink <jasper.burkink@specializedbrainsict.nl>
2023-08-17 10:19:55 +0200

    Review remarks

--- CSV-Com/webapp_old/src/components/button.jsx
+++ CSV-Com/webapp_old/src/components/button.jsx
@@ -2,6 +2,6 @@ import './button.scss';
 
 export function Button(props) {
   return (
-    <a className="btn" href={props.href}>{props.text}</a>
+      <a className="btn" href={props.href ? props.href : undefined}>{props.text}</a>
   );
 }
