6db96225cae765c4f07031c1c91f2738126fdfcd
Anoniem <anoniem@specializedbrainsict.nl>
2023-10-16 12:15:00 +0200

    message weggehaald bij exceptions

--- CSV-Com/API/Controllers/MaritalStatusController.cs
+++ CSV-Com/API/Controllers/MaritalStatusController.cs
@@ -26,7 +26,7 @@ namespace API.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, ex);
             }
         }
 
