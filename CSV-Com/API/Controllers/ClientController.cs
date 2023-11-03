691f1e141345950634b826ec26c55829f00958e9
Jasper Burkink <jasper.burkink@specializedbrainsict.nl>
2023-11-03 11:44:59 +0100

    Final

--- CSV-Com/API/Controllers/ClientController.cs
+++ CSV-Com/API/Controllers/ClientController.cs
@@ -2,6 +2,7 @@
 using Application.Clients.Commands.CreateClient;
 using Application.Clients.Commands.DeleteClientDriversLicence;
 using Application.Clients.Queries.GetClients;
+using Application.Clients.Queries.SearchClients;
 using Application.Common.Interfaces.CVS;
 using Domain.CVS.Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -64,6 +65,21 @@ namespace API.Controllers
             await Mediator.Send(command);
 
             return NoContent();
+        }
+
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<SearchClientDto>>> SearchClients([FromQuery] SearchClientsQuery query)
+        {
+            try
+            {
+                var clients = await Mediator.Send(query);
+                return Ok(clients);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
     }
 }
---
+++ CSV-Com/Application/Clients/Queries/SearchClients/SearchClientDto.cs
@@ -0,0 +1,30 @@
+﻿using Application.Clients.Queries.GetClients;
+using Application.Common.Mappings;
+using AutoMapper;
+using Domain.CVS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Clients.Queries.SearchClients
+{
+    public class SearchClientDto : IMapFrom<Client>
+    {
+        public int IdentificationNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Initials { get; set; }
+
+        public string PrefixLastName { get; set; }
+
+        public string LastName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Client, SearchClientDto>();
+        }
+    }
+}
