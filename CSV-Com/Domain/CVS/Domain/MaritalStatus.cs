be991982731add415c3ff3456a7ee4e33525c186
Anoniem <anoniem@specializedbrainsict.nl>
2023-11-17 16:39:09 +0100

    onnodige usings weggehaald

--- CSV-Com/Domain/CVS/Domain/MaritalStatus.cs
+++ CSV-Com/Domain/CVS/Domain/MaritalStatus.cs
@@ -1,10 +1,4 @@
 ﻿using Domain.Common;
-using Domain.CVS.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.CVS.Domain
 {
