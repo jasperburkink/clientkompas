f2b2f0cd939104aa7fc1e78c3b3ff58a905529d8
Anoniem <anoniem@specializedbrainsict.nl>
2024-01-15 12:20:47 +0100

    re-edited button.tsx

--- CSV-Com/webapp/src/components/common/button.tsx
+++ CSV-Com/webapp/src/components/common/button.tsx
@@ -3,14 +3,26 @@ import './button.css';
 import { ButtonType } from '../../types/common/ButtonComponentType';
 import { getClassNameButtonType } from '../../types/common/ButtonComponentType';
 
-interface ButtonProps extends React.HtmlHTMLAttributes<HTMLButtonElement> {
-    text: string,
-    buttonType: ButtonType
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  buttonType: ButtonType;
+  isOpen?: boolean; 
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const Button = (props: ButtonProps) => (
-  <button
-    className={props.className + ' ' + getClassNameButtonType(props.buttonType.type)} >
+export const Button = (props: ButtonProps) => {
+  const handleClick = () => {
+    if (props.setIsOpen) {
+      props.setIsOpen((prevIsOpen) => !prevIsOpen);
+    }
+  };
+
+  return (
+    <button
+      className={props.className + ' ' + getClassNameButtonType(props.buttonType.type)}
+      onClick={handleClick} // Add the click handler
+    >
       {props.text}
-  </button>
-);
+    </button>
+  );
+};
