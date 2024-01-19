637e0a92941706741363f69228d4ea2d310510ff
Anoniem <anoniem@specializedbrainsict.nl>
2024-01-19 09:48:05 +0100

    aanpassingen na comments jasper

--- CSV-Com/webapp/src/components/common/password-field.tsx
+++ CSV-Com/webapp/src/components/common/password-field.tsx
@@ -7,17 +7,19 @@ interface IPasswordFieldProps extends React.HtmlHTMLAttributes<HTMLElement> {
     inputfieldname: string;
     placeholder: string;
 }
+const type1 = 'password';
+const type2 = 'text';
 
 const PasswordField = (props: IPasswordFieldProps) => {
     const [icon,setIcon] = useState(<FontAwesomeIcon icon={faEyeSlash} size="lg" style={{color: "#000000",}} />);
-    const [type,setType] = useState('password');
+    const [type,setType] = useState(type1);
     
     function visibility() {
-        if (type === "password") {
-            setType('text');
+        if (type === type1) {
+            setType(type2);
             setIcon(<FontAwesomeIcon icon={faEye} size="lg" style={{color: "#000000",}} />);
         } else {
-            setType('password')
+            setType(type1);
             setIcon(<FontAwesomeIcon icon={faEyeSlash} size="lg" style={{color: "#000000",}} />)
         }
         
