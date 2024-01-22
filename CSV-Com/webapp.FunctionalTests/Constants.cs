650285cf7ecedadb89602177cb87c7c56c736302
Jasper Burkink <jasper.burkink@specializedbrainsict.nl>
2024-01-22 14:59:11 +0100

    Final

--- CSV-Com/webapp.FunctionalTests/Constants.cs
+++ CSV-Com/webapp.FunctionalTests/Constants.cs
@@ -2,6 +2,10 @@
 {
     internal static class Constants
     {
+#if DEBUG
         public const string Url = "http://localhost:3000/";
+#else
+        public static readonly string Url = Environment.GetEnvironmentVariable("WebAppUrl");
+#endif
     }
 }
