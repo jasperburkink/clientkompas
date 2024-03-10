a2a4cf73ac98e581edfcc6ab535fe9d878dce1f8
Anoniem <anoniem@specializedbrainsict.nl>
2024-03-10 22:47:59 +0545

    Added build date at the bottom of the application to more easily test if the pipeline works

--- CSV-Com/webapp/src/index.tsx
+++ CSV-Com/webapp/src/index.tsx
@@ -4,20 +4,24 @@ import './index.css';
 import App from './pages/App';
 import Clients from './pages/Clients';
 import reportWebVitals from './utils/reportWebVitals';
+import preval from 'preval.macro'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route  path='/Clients' element={<Clients />} />
-                <Route path='/Clients/:id' element={<Clients />} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
+    <>
+        <React.StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<App />} />
+                    <Route  path='/Clients' element={<Clients />} />
+                    <Route path='/Clients/:id' element={<Clients />} />
+                </Routes>
+            </BrowserRouter>
+        </React.StrictMode>
+        <span>Build Date: {preval`module.exports = new Date().toLocaleString();`}.</span>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
