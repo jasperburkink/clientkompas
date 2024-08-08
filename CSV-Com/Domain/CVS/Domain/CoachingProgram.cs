ecf9f568874396f15fd1e834cef3a357823a4349
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2024-08-08 14:55:00 +0200

    Domain

---
+++ CSV-Com/Domain/CVS/Domain/CoachingProgram.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using Domain.CVS.Enums;
+
+namespace Domain.CVS.Domain
+{
+    public class CoachingProgram : BaseAuditableEntity
+    {
+        public required string Title { get; set; }
+
+        public required string OrderNumber { get; set; }
+
+        public Organization? Organization { get; set; }
+
+        public int? OrganizationId { get; set; }
+
+        public required CoachingProgramType CoachingProgramType { get; set; }
+
+        public required DateOnly BeginDate { get; set; }
+
+        public required DateOnly EndDate { get; set; }
+
+        public required decimal BudgetAmmount { get; set; }
+
+        public required decimal HourlyRate { get; set; }
+    }
+}
---
+++ CSV-Com/Domain/CVS/Enums/CoachingProgramType.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.CVS.Enums
+{
+    public enum CoachingProgramType
+    {
+        PGB,
+        InternJobCoach,
+        ExternJobCoach,
+        PrivateCoachingProgram,
+        CommercialCoachingProgram
+    }
+}
