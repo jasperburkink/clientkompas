89412f8e94fc9d6d2f8e0bf43289f00e9449be3f
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2024-09-13 15:21:37 +0200

    Unittests repair

--- CSV-Com/Application.UnitTests/Authentication/Commands/Login/LoginCommandDtoTests.cs
+++ CSV-Com/Application.UnitTests/Authentication/Commands/Login/LoginCommandDtoTests.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Authentication.Commands.Login;
 using Application.Common.Interfaces.Authentication;
+using Application.Common.Models;
+using Domain.Authentication.Domain;
 using Moq;
 
 namespace Application.UnitTests.Authentication.Commands.Login
@@ -12,11 +14,18 @@ namespace Application.UnitTests.Authentication.Commands.Login
         {
             // Arrange
             var isUserLoggedIn = true;
+            var user = new AuthenticationUser()
+            {
+                Salt = new byte[0]
+            };
+            var loggedInUser = new LoggedInResult(isUserLoggedIn, user, new List<string>());
 
             var identityServiceMock = new Mock<IIdentityService>();
-            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(isUserLoggedIn);
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
 
+            var bearerToken = "test";
             var bearerTokenServiceMock = new Mock<IBearerTokenService>();
+            bearerTokenServiceMock.Setup(btsm => btsm.GenerateBearerTokenAsync(It.IsAny<AuthenticationUser>(), It.IsAny<IList<string>>())).ReturnsAsync(bearerToken);
 
             var handler = new LoginCommandHandler(identityServiceMock.Object, bearerTokenServiceMock.Object);
 
@@ -38,9 +47,10 @@ namespace Application.UnitTests.Authentication.Commands.Login
         {
             // Arrange
             var isUserLoggedIn = false;
+            var loggedInUser = new LoggedInResult(isUserLoggedIn);
 
             var identityServiceMock = new Mock<IIdentityService>();
-            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(isUserLoggedIn);
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
 
             var bearerTokenServiceMock = new Mock<IBearerTokenService>();
 
@@ -57,6 +67,67 @@ namespace Application.UnitTests.Authentication.Commands.Login
 
             // Assert
             result.Success.Should().Be(isUserLoggedIn);
+        }
+
+        [Fact]
+        public async Task Handle_Login_BearerTokenShouldNotBeEmpty()
+        {
+            // Arrange
+            var isUserLoggedIn = true;
+            var user = new AuthenticationUser()
+            {
+                Salt = new byte[0]
+            };
+            var loggedInUser = new LoggedInResult(isUserLoggedIn, user, new List<string>());
+
+            var identityServiceMock = new Mock<IIdentityService>();
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
+
+            var bearerToken = "test";
+            var bearerTokenServiceMock = new Mock<IBearerTokenService>();
+            bearerTokenServiceMock.Setup(btsm => btsm.GenerateBearerTokenAsync(It.IsAny<AuthenticationUser>(), It.IsAny<IList<string>>())).ReturnsAsync(bearerToken);
+
+            var handler = new LoginCommandHandler(identityServiceMock.Object, bearerTokenServiceMock.Object);
+
+            var command = new LoginCommand
+            {
+                UserName = "Test",
+                Password = "Test1234"
+            };
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            result.BearerToken.Should().NotBeEmpty().And.Be(bearerToken);
+        }
+
+        [Fact]
+        public async Task Handle_Login_BearerTokenShouldBeNull()
+        {
+            // Arrange
+            var isUserLoggedIn = false;
+            var loggedInUser = new LoggedInResult(isUserLoggedIn);
+
+            var identityServiceMock = new Mock<IIdentityService>();
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
+
+            var bearerTokenServiceMock = new Mock<IBearerTokenService>();
+            bearerTokenServiceMock.Setup(btsm => btsm.GenerateBearerTokenAsync(It.IsAny<AuthenticationUser>(), It.IsAny<IList<string>>())).ReturnsAsync(string.Empty);
+
+            var handler = new LoginCommandHandler(identityServiceMock.Object, bearerTokenServiceMock.Object);
+
+            var command = new LoginCommand
+            {
+                UserName = "Test",
+                Password = "Test1234"
+            };
+
+            // Act
+            var result = await handler.Handle(command, default);
+
+            // Assert
+            result.BearerToken.Should().BeNull();
         }
     }
 }
--- CSV-Com/Application.UnitTests/Authentication/Commands/Login/LoginCommandTests.cs
+++ CSV-Com/Application.UnitTests/Authentication/Commands/Login/LoginCommandTests.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Authentication.Commands.Login;
 using Application.Common.Interfaces.Authentication;
+using Application.Common.Models;
+using Domain.Authentication.Domain;
 using Moq;
 
 namespace Application.UnitTests.Authentication.Commands.Login
@@ -11,9 +13,18 @@ namespace Application.UnitTests.Authentication.Commands.Login
         {
             // Arrange
             var identityServiceMock = new Mock<IIdentityService>();
-            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
+            var isUserLoggedIn = true;
+            var user = new AuthenticationUser()
+            {
+                Salt = new byte[0]
+            };
+            var loggedInUser = new LoggedInResult(isUserLoggedIn, user, new List<string>());
 
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
+
+            var bearerToken = "test";
             var bearerTokenServiceMock = new Mock<IBearerTokenService>();
+            bearerTokenServiceMock.Setup(btsm => btsm.GenerateBearerTokenAsync(It.IsAny<AuthenticationUser>(), It.IsAny<IList<string>>())).ReturnsAsync(bearerToken);
 
             var handler = new LoginCommandHandler(identityServiceMock.Object, bearerTokenServiceMock.Object);
 
@@ -23,19 +34,28 @@ namespace Application.UnitTests.Authentication.Commands.Login
                 Password = "Test1234"
             };
 
+            var loginCommandDto = new LoginCommandDto
+            {
+                Success = isUserLoggedIn,
+                BearerToken = bearerToken
+            };
+
             // Act
             var result = await handler.Handle(command, default);
 
             // Assert
-            result.Success.Should().BeTrue();
+            result.Should().BeEquivalentTo(loginCommandDto);
         }
 
         [Fact]
         public async Task Handle_LoginUser_ShouldReturnFalse()
         {
             // Arrange
+            var isUserLoggedIn = false;
+            var loggedInUser = new LoggedInResult(isUserLoggedIn);
+
             var identityServiceMock = new Mock<IIdentityService>();
-            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(false);
+            identityServiceMock.Setup(mis => mis.LoginAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(loggedInUser);
 
             var bearerTokenServiceMock = new Mock<IBearerTokenService>();
 
@@ -47,11 +67,16 @@ namespace Application.UnitTests.Authentication.Commands.Login
                 Password = "Test1234"
             };
 
+            var loginCommandDto = new LoginCommandDto
+            {
+                Success = isUserLoggedIn
+            };
+
             // Act
             var result = await handler.Handle(command, default);
 
             // Assert
-            result.Success.Should().BeFalse();
+            result.Should().BeEquivalentTo(loginCommandDto);
         }
     }
 }
