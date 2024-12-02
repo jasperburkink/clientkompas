2d4b3eefc5536bd2ff95862babcc1751d8c72ace
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2024-12-02 11:04:37 +0100

    temp

--- CSV-Com/Application.FunctionalTests/Authentication/Commands/ResendTwoFactorAuthenticationToken/ResendTwoFactorAuthenticationTokenCommandDtoTests.cs
+++ CSV-Com/Application.FunctionalTests/Authentication/Commands/ResendTwoFactorAuthenticationToken/ResendTwoFactorAuthenticationTokenCommandDtoTests.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.FunctionalTests.Authentication.Commands.ResendTwoFactorAuthenticationToken
+﻿namespace Application.FunctionalTests.Authentication.Commands.ResendTwoFactorAuthenticationToken
 {
-    internal class ResendTwoFactorAuthenticationTokenCommandDtoTests
+    public class ResendTwoFactorAuthenticationTokenCommandDtoTests : BaseTestFixture
     {
     }
 }
