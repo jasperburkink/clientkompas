ff1115be5b423f306da3adf288508fc886a24b6e
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2024-12-10 10:20:00 +0100

    Review remarks

--- CSV-Com/Application/Authentication/Commands/Login/LoginCommand.cs
+++ CSV-Com/Application/Authentication/Commands/Login/LoginCommand.cs
@@ -15,8 +15,6 @@ namespace Application.Authentication.Commands.Login
 
     public class LoginCommandHandler : IRequestHandler<LoginCommand, LoginCommandDto>
     {
-        private const string RESOURCE_KEY_INVALIDLOGIN = "InvalidLogin";
-        private const string RESOURCE_KEY_NOEMAILADDRESS = "NoEmailAddress";
         private readonly IIdentityService _identityService;
         private readonly IBearerTokenService _bearerTokenService;
         private readonly ITokenService _tokenService;
@@ -38,45 +36,51 @@ namespace Application.Authentication.Commands.Login
 
             if (IsInvalidLogin(loggedInUser))
             {
-                throw new InvalidLoginException(_resourceMessageProvider.GetMessage(typeof(LoginCommandHandler), RESOURCE_KEY_INVALIDLOGIN));
+                throw new InvalidLoginException(_resourceMessageProvider.GetMessage(typeof(LoginCommandHandler), AuthenticationCommandContants.RESOURCE_KEY_INVALIDLOGIN));
             }
 
-            if (loggedInUser.User.TwoFactorEnabled)
+            return loggedInUser.User!.TwoFactorEnabled
+                ? await HandleTwoFactorAuthentication(loggedInUser)
+                : await HandleStandardLogin(loggedInUser);
+        }
+
+        private async Task<LoginCommandDto> HandleStandardLogin(LoggedInResult loggedInUser)
+        {
+            var bearerToken = await _bearerTokenService.GenerateBearerTokenAsync(loggedInUser.User, loggedInUser.Roles); // UserInfo & roles are processed inside the bearertoken claims
+            var refreshToken = await _tokenService.GenerateTokenAsync(loggedInUser.User, nameof(LoginCommandDto.RefreshToken));
+
+            return new LoginCommandDto
             {
-                // Token value that user needs to enter
-                var twoFactorAuthenticationTokenValue = await _identityService.Get2FATokenAsync(loggedInUser.User.Id);
+                Success = true,
+                BearerToken = bearerToken,
+                RefreshToken = refreshToken
+            };
+        }
 
-                if (string.IsNullOrEmpty(loggedInUser.User.Email))
-                {
-                    throw new NotFoundException(_resourceMessageProvider.GetMessage(typeof(LoginCommandHandler), RESOURCE_KEY_NOEMAILADDRESS));
-                }
+        private async Task<LoginCommandDto> HandleTwoFactorAuthentication(LoggedInResult loggedInUser)
+        {
+            // Token value that user needs to enter
+            var twoFactorAuthenticationTokenValue = await _identityService.Get2FATokenAsync(loggedInUser.User.Id);
 
-                // Security token for checking loginstatus user
-                var twoFactorPendingTokenValue = await _tokenService.GenerateTokenAsync(loggedInUser.User, nameof(LoginCommandDto.TwoFactorPendingToken));
-
-                // Send the token via email TODO: user emailmodule
-                await _emailService.SendEmailAsync(loggedInUser.User.Email, "Two-factor authentication token", twoFactorAuthenticationTokenValue);
-
-                return new LoginCommandDto
-                {
-                    Success = true,
-                    UserId = loggedInUser.User.Id,
-                    TwoFactorPendingToken = twoFactorPendingTokenValue,
-                    ExpiresAt = DateTime.UtcNow.Add(TwoFactorPendingTokenConstants.TOKEN_TIMEOUT)
-                };
-            }
-            else
+            if (string.IsNullOrEmpty(loggedInUser.User.Email))
             {
-                var bearerToken = await _bearerTokenService.GenerateBearerTokenAsync(loggedInUser.User, loggedInUser.Roles); // UserInfo & roles are processed inside the bearertoken claims
-                var refreshToken = await _tokenService.GenerateTokenAsync(loggedInUser.User, nameof(LoginCommandDto.RefreshToken));
-
-                return new LoginCommandDto
-                {
-                    Success = true,
-                    BearerToken = bearerToken,
-                    RefreshToken = refreshToken
-                };
+                throw new NotFoundException(_resourceMessageProvider.GetMessage(typeof(LoginCommandHandler), AuthenticationCommandContants.RESOURCE_KEY_NOEMAILADDRESS));
             }
+
+            // Security token for checking loginstatus user
+            var twoFactorPendingTokenValue = await _tokenService.GenerateTokenAsync(loggedInUser.User, nameof(LoginCommandDto.TwoFactorPendingToken));
+
+            // Send the token via email
+            // TODO: user emailmodule            
+            await _emailService.SendEmailAsync(loggedInUser.User.Email, "Two-factor authentication token", twoFactorAuthenticationTokenValue);
+
+            return new LoginCommandDto
+            {
+                Success = true,
+                UserId = loggedInUser.User.Id,
+                TwoFactorPendingToken = twoFactorPendingTokenValue,
+                ExpiresAt = DateTime.UtcNow.Add(TwoFactorPendingTokenConstants.TOKEN_TIMEOUT)
+            };
         }
 
         private static bool IsInvalidLogin(LoggedInResult loggedInUser)
