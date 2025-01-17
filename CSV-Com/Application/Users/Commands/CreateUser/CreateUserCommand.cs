1914ec5d08de1dedce2c4ecd94d38c2b57ab5650
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-01-17 12:28:36 +0100

    Added command

---
+++ CSV-Com/Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Security;
+using Domain.Authentication.Constants;
+
+namespace Application.Users.Commands.CreateUser
+{
+    [Authorize(Policy = Policies.UserManagement)]
+    public record CreateUserCommand : IRequest<CreateUserCommand>
+    {
+        public int UserId { get; set; }
+
+
+    }
+}
