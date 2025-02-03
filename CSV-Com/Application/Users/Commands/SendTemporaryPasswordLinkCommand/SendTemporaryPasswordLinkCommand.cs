780e6405c53185d5393b1c0393efeac18fdff253
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-02-03 10:22:19 +0100

    init

---
+++ CSV-Com/Application/Users/Commands/SendTemporaryPasswordLinkCommand/SendTemporaryPasswordLinkCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Models;
+using Application.Common.Security;
+using Domain.Authentication.Constants;
+
+namespace Application.Users.Commands.SendTemporaryPasswordLinkCommand
+{
+    [Authorize(Policy = Policies.UserManagement)]
+    public record SendTemporaryPasswordLinkCommand : IRequest<Result<SendTemporaryPasswordLinkCommandDto>>
+    {
+        public string EmailAddress { get; set; }
+    }
+}
