ce81850c2888ead75359884e0ec6cd6251d678c7
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-02-03 14:32:37 +0100

    Refactor

--- CSV-Com/Application/Users/Commands/SendTemporaryPasswordLinkCommand/SendTemporaryPasswordLinkCommand.cs
+++ CSV-Com/Application/Users/Commands/SendTemporaryPasswordLinkCommand/SendTemporaryPasswordLinkCommand.cs
@@ -6,6 +6,8 @@ using Application.Common.Models;
 using Application.Common.Security;
 using Ardalis.GuardClauses;
 using Domain.Authentication.Constants;
+using Domain.Authentication.Domain;
+using Domain.CVS.Domain;
 
 namespace Application.Users.Commands.SendTemporaryPasswordLinkCommand
 {
@@ -44,62 +46,86 @@ namespace Application.Users.Commands.SendTemporaryPasswordLinkCommand
             // When token has been sent more than n times, send temporary password link with token to user.
             if (authenticationUser.TemporaryPasswordTokenCount <= MAX_TIMES_SEND_TEMP_PASSWORD_TOKEN)
             {
-                var newToken = await tokenService.GenerateTokenAsync(authenticationUser, "TemporaryPasswordToken"); // TODO: name in constants
-                var link = new Uri($"https://localhost:3000/ChangePassword/{WebUtility.UrlEncode(newToken)}");
+                var result = await HandleResendTemporaryPasswordLink(identityService, tokenService, emailService, authenticationUser, cvsUser);
 
-                var emailAddress = authenticationUser.UserName ?? authenticationUser.Email;
-
-                if (string.IsNullOrEmpty(emailAddress))
+                if (!result.Succeeded)
                 {
-                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure("No emailaddress found for this user.");
+                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure(result.Errors);
                 }
-
-                await emailService.SendEmailAsync(emailAddress, "Gebruikersgegevens",
-                    $"""
-                    Beste {cvsUser.FirstName},
-
-                    Het eerder verstuurde wachtwoord is tijdelijk en dient zo spoedig mogelijk te worden aangepast.
-                    Via de volgende link kunt u uw tijdelijke wachtwoord aanpassen: {link}
-                    """); // TODO: Change to new emailmodule
-
-                authenticationUser.TemporaryPasswordTokenCount++;
-
-                await identityService.UpdateUserAsync(authenticationUser);
             }
             // Otherwise send contact info from user that created this user.
             else
             {
-                if (cvsUser == null)
+                var result = await HandleUserContactInformation(emailService, cvsUser);
+
+                if (!result.Succeeded)
                 {
-                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure("User not found.");
+                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure(result.Errors);
                 }
-
-                if (cvsUser.CreatedByUser == null)
-                {
-                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure("User which create this user not found.");
-                }
-
-                var emailAddress = cvsUser.CreatedByUser.EmailAddress;
-
-                if (string.IsNullOrEmpty(emailAddress))
-                {
-                    return Result<SendTemporaryPasswordLinkCommandDto>.Failure("No emailaddress found for this user.");
-                }
-
-                await emailService.SendEmailAsync(emailAddress, "Gebruikersgegevens",
-                    $"""
-                    Beste {cvsUser.FirstName},
-
-                    Uw link om uw tijdelijke wachtwoord te kunnen resetten is helaas verlopen.
-                    Mocht u een nieuwe link willen ontvangen, dan verzoeken wij u om contact op te nemen met {cvsUser.CreatedByUser.FirstName} {cvsUser.PrefixLastName + " " ?? ""}{cvsUser.LastName}.
-                    Dat kan via het volgende e-mailadres: {emailAddress}
-                    """); // TODO: Change to new emailmodule
             }
 
             return Result.Success(new SendTemporaryPasswordLinkCommandDto
             {
                 UserId = cvsUser.Id
             });
+        }
+
+        private static async Task<Result> HandleResendTemporaryPasswordLink(IIdentityService identityService, ITokenService tokenService, IEmailService emailService, AuthenticationUser authenticationUser, User cvsUser)
+        {
+            var newToken = await tokenService.GenerateTokenAsync(authenticationUser, "TemporaryPasswordToken"); // TODO: name in constants
+            var link = new Uri($"https://localhost:3000/ChangePassword/{WebUtility.UrlEncode(newToken)}");
+
+            var emailAddress = authenticationUser.UserName ?? authenticationUser.Email;
+
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                return Result.Failure("No emailaddress found for this user.");
+            }
+
+            await emailService.SendEmailAsync(emailAddress, "Gebruikersgegevens",
+                $"""
+                    Beste {cvsUser.FirstName},
+
+                    Het eerder verstuurde wachtwoord is tijdelijk en dient zo spoedig mogelijk te worden aangepast.
+                    Via de volgende link kunt u uw tijdelijke wachtwoord aanpassen: {link}
+                    """); // TODO: Change to new emailmodule
+
+            authenticationUser.TemporaryPasswordTokenCount++;
+
+            await identityService.UpdateUserAsync(authenticationUser);
+
+            return Result.Success();
+        }
+
+        private static async Task<Result> HandleUserContactInformation(IEmailService emailService, User cvsUser)
+        {
+            if (cvsUser == null)
+            {
+                return Result.Failure("User not found.");
+            }
+
+            if (cvsUser.CreatedByUser == null)
+            {
+                return Result.Failure("User which create this user not found.");
+            }
+
+            var emailAddress = cvsUser.CreatedByUser.EmailAddress;
+
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                return Result.Failure("No emailaddress found for this user.");
+            }
+
+            await emailService.SendEmailAsync(emailAddress, "Gebruikersgegevens",
+                $"""
+                    Beste {cvsUser.FirstName},
+
+                    Uw link om uw tijdelijke wachtwoord te kunnen resetten is helaas verlopen.
+                    Mocht u een nieuwe link willen ontvangen, dan verzoeken wij u om contact op te nemen met {cvsUser.CreatedByUser.FirstName} {cvsUser.PrefixLastName + " " ?? ""}{cvsUser.LastName}.
+                    Dat kan via het volgende e-mailadres: {emailAddress}
+                    """); // TODO: Change to new emailmodule
+
+            return Result.Success();
         }
     }
 }
