d2bc1ad99ea88cc595076792d8a63985c5982c3f
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-02-07 09:49:25 +0100

    Query

---
+++ CSV-Com/Application/Users/Queries/SearchUsers/SearchUsersQuery.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces.CVS;
+using Application.Common.Security;
+using Domain.Authentication.Constants;
+
+namespace Application.Users.Queries.SearchUsers
+{
+    [Authorize(Policy = Policies.ClientRead)]
+    public record SearchUsersQuery : IRequest<IEnumerable<SearchUsersQueryDto>>
+    {
+        public string? SearchTerm { get; init; };
+    }
+
+    public class SearchUsersQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<SearchUsersQuery, IEnumerable<SearchUsersQueryDto>>
+    {
+        public async Task<IEnumerable<SearchUsersQueryDto>> Handle(SearchUsersQuery request, CancellationToken cancellationToken)
+        {
+            return [.. (await unitOfWork.UserRepository.FullTextSearch(request.SearchTerm, cancellationToken, user => user.FullName))
+                .AsQueryable()
+                .ProjectTo<SearchUsersQueryDto>(mapper.ConfigurationProvider)
+                .OrderBy(user => user.LastName)
+                .ThenBy(user => user.FirstName)];
+        }
+    }
+}
