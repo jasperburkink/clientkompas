d5ea7dd15c5e4abced7ee5cf84f7f5c461be615e
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-02-18 11:40:21 +0100

    Intergration tests

---
+++ CSV-Com/Application.FunctionalTests/Users/Commands/DeactivateUser/DeactivateUserCommandTests.cs
@@ -0,0 +1,89 @@
+﻿using Application.Users.Commands.DeactivateUser;
+using Domain.Authentication.Constants;
+using Domain.CVS.Domain;
+using TestData;
+using TestData.User;
+
+namespace Application.FunctionalTests.Users.Commands.DeactivateUser
+{
+    public class DeactivateUserCommandTests : BaseTestFixture
+    {
+        private ITestDataGenerator<User> _testDataGeneratorUser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testDataGeneratorUser = new UserDataGenerator();
+        }
+
+        [Test]
+        public async Task Handle_CorrectFlow_ShouldSendReturnSuccess()
+        {
+            // Arrange
+            var user = _testDataGeneratorUser.Create();
+
+            await RunAsAsync(Roles.Administrator);
+
+            await AddAsync(user);
+
+            var command = new DeactivateUserCommand
+            {
+                Id = user.Id
+            };
+
+            // Act
+            var result = await SendAsync(command);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Succeeded.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Handle_UserDoesNotExist_ShouldReturnFailure()
+        {
+            // Arrange
+            var user = _testDataGeneratorUser.Create();
+
+            await RunAsAsync(Roles.Administrator);
+
+            var command = new DeactivateUserCommand
+            {
+                Id = user.Id
+            };
+
+            // Act
+            var result = await SendAsync(command);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Succeeded.Should().BeFalse();
+            result.ErrorMessage.Should().Be($"Gebruiker met id '${command.Id}' kan niet worden gevonden.");
+        }
+
+        [Test]
+        public async Task Handle_UserIsAlreadyDeactivated_ShouldReturnFailure()
+        {
+            // Arrange
+            var user = _testDataGeneratorUser.Create();
+            user.Deactivate(DateTime.UtcNow);
+
+            await RunAsAsync(Roles.Administrator);
+
+            await AddAsync(user);
+
+            var command = new DeactivateUserCommand
+            {
+                Id = user.Id
+            };
+
+            // Act
+            var result = await SendAsync(command);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Succeeded.Should().BeFalse();
+            result.ErrorMessage.Should().Be($"Gebruiker met id '${user.Id}' is al gedeactiveerd op ${user.DeactivationDateTime} en kan dus niet opnieuw worden gedeactiveerd.");
+        }
+    }
+}
