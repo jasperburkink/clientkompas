fd0f763ee02013b95f0d2244dedcc97887201381
jasper.burkink <jasper.burkink@specializedbrainsict.nl>
2025-02-18 12:29:34 +0100

    begin

---
+++ CSV-Com/Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Security;
+using Domain.Authentication.Constants;
+
+namespace Application.Users.Queries.GetUser
+{
+    [Authorize(Policy = Policies.UserRead)]
+    public class GetUserQuery :
+    {
+    }
+}
